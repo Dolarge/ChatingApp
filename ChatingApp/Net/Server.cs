7e0841b1f9d06d3a29f76d8979f755d8055c1ab9
dh.cheon <dohyun722@daum.net>
2022-02-11 21:09:12 +0900

    feat: RelayCommand

---
+++ ChatingApp/Net/Server.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.Net
+{
+    public class Server
+    {
+        TcpClient _client;
+
+        public Server()
+        {
+            _client= new TcpClient();
+
+        }
+
+
+        //if not connected Server, Connect Server
+        public void ConnectToServer()
+        {
+            if (!_client.Connected)
+            {
+                _client.Connect("127.0.0.1", 7891);
+            }
+        }
+    }
+}
