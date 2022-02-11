1ee34a3a5d5fe0ecf103370063eaaffc0eb70acc
dh.cheon <dohyun722@daum.net>
2022-02-11 21:12:12 +0900

    feat : Update ConnectTo Server

--- ChatingApp/MVVM/ViewModel/MainViewModel.cs
+++ ChatingApp/MVVM/ViewModel/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ChatClient.MVVM.Core;
+using ChatClient.Net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,16 @@ using System.Threading.Tasks;
 
 namespace ChatClient.MVVM.ViewModel
 {
-    public class MainViewModel
+    class MainViewModel
     {
 
+        public RelayCommand ConnectToServerCommand { get; set; }
+
+        private Server _server;
+        public MainViewModel()
+        {
+            _server = new Server();
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer());
+        }
     }
 }
