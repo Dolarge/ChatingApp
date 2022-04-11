d1ef812c1bd4edc63a7e3d53ff28c33818778bf4
dh.cheon <dohyun722@daum.net>
2022-04-11 19:26:10 +0900

    feat : Binding ListView

--- ChatingApp/MVVM/ViewModel/MainViewModel.cs
+++ ChatingApp/MVVM/ViewModel/MainViewModel.cs
@@ -1,15 +1,19 @@
 ﻿using ChatClient.MVVM.Core;
+using ChatClient.MVVM.Model;
 using ChatClient.Net;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ChatClient.MVVM.ViewModel
 {
-    class MainViewModel
+    public class MainViewModel
     {
+        public ObservableCollection<UserModel> Users { get; set; }
 
         public RelayCommand ConnectToServerCommand { get; set; }
         public string UserName { get;set; }
@@ -17,6 +21,7 @@ namespace ChatClient.MVVM.ViewModel
         private Server _server;
         public MainViewModel()
         {
+            Users =  new ObservableCollection<UserModel>();
             _server = new Server();
             _server.connectedEvent += UserConnected;
             //if UserName is empty, cant Run RelayCommand
@@ -25,7 +30,18 @@ namespace ChatClient.MVVM.ViewModel
 
         private void UserConnected()
         {
-            throw new NotImplementedException();
+            var user = new UserModel
+            {
+                UserName = _server.packetReader.ReadMessage(),
+                UID = _server.packetReader.ReadMessage()
+            };
+
+
+            if (!Users.Any(x=>x.UID == user.UID))
+            {
+                Application.Current.Dispatcher.Invoke(() => Users.Add(user));
+            }
+
         }
     }
 }
